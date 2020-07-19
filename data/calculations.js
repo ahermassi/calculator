ad148c4257d0ca22d150d92a4eccd8ee7591b929
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-07-19 19:23:06 -0400

    Remove unused db functions

--- data/calculations.js
+++ data/calculations.js
@@ -1,12 +1,11 @@
 const mongoCollections = require('../config/mongoCollections');
 const calculations = mongoCollections.calculations;
-const { ObjectId } = require('mongodb');
 
 module.exports = {
     async addCalculation(calculation, result, timestamp) {
-        if(!calculation || typeof calculation!= 'string') throw 'you must provide a valid calculation string';
-        if(result === null || typeof result!= 'number') throw 'you must provide a valid calculation result';
-        if(!timestamp) throw 'you must provide a valid timestamp';
+        if(!calculation || typeof calculation!= 'string') throw 'You must provide a valid calculation string';
+        if(result === null || typeof result!= 'number') throw 'You must provide a valid calculation result';
+        if(!timestamp) throw 'You must provide a valid timestamp';
 
         const calculationsCollection = await calculations();
         let newCalculation = {
@@ -16,29 +15,10 @@ module.exports = {
             };
         const insertInfo = await calculationsCollection.insertOne(newCalculation);
         if (insertInfo.insertedCount === 0) throw 'Could not add calculation';
-        const newId = insertInfo.insertedId;
-        return await this.getCalculation(newId);
     },
-
-    async getAllCalculations() {
-        const calculationsCollection = await calculations();
-        return calculationsCollection.find({}).toArray();
-    },
-
-    async getCalculation(id) {
-        if (!id) throw 'You must provide a calculation id to search for';
-
-        const objId = ObjectId(id);
-        const calculationsCollection = await calculations();
-        const calculation = await calculationsCollection.findOne({ _id: objId });
-        if (calculation === null) throw 'No calculation with this id';
-
-        return calculation;
-    },
-
     async getCalculationsByLimit(limit) {
         const calculationsCollection = await calculations();
-        return calculationsCollection.find().sort({timestamp: -1}).limit(10).toArray();
+        return calculationsCollection.find().sort({timestamp: -1}).limit(limit).toArray();
     }
 };
 
