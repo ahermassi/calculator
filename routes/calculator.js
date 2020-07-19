ac7b4d1a694906814f3ec851bd80d172b6133879
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-07-18 21:09:41 -0400

    Create a dumb calculator

--- routes/calculator.js
+++ routes/calculator.js
@@ -5,4 +5,10 @@ router.get('/', async (req, res) => {
     res.render('calculator', {title: "The Best Calculator in the World!"});
 });
 
+router.post('/', async (req, res) => {
+    let data = req.body;
+    let firstOperand = data['first-operand'], secondOperand = data['second-operand'], operator = data.operator;
+    console.log(parseFloat(firstOperand) + parseFloat(secondOperand));
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const calculatorRoutes = require('./calculator');
 
 const constructorMethod = (app) => {
     app.use('/', calculatorRoutes);
+    app.use('/calculator', calculatorRoutes);
     app.use('*', (req, res) => {
         res.redirect('/');
     });
